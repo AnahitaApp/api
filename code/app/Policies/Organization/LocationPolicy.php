0150116bee2bf16354b76e121b05bbb9918fe2bf
Bryce Meyer <bryce13950@gmail.com>
2020-05-24 04:09:03 +0200

    created location policy

---
+++ code/app/Policies/Organization/LocationPolicy.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Policies\Organization;
+
+use App\Policies\BaseBelongsToOrganizationPolicyAbstract;
+
+/**
+ * Class OrganizationManagerPolicy
+ * @package App\Policies\Organization
+ */
+class LocationPolicy extends BaseBelongsToOrganizationPolicyAbstract
+{}
---
+++ code/tests/Integration/Policies/Organization/LocationPolicyTest.php
@@ -0,0 +1,159 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Integration\Policies\Organization;
+
+use App\Models\Organization\Location;
+use App\Models\Organization\Organization;
+use App\Models\Organization\OrganizationManager;
+use App\Models\Role;
+use App\Models\User\User;
+use App\Policies\Organization\LocationPolicy;
+use Tests\DatabaseSetupTrait;
+use Tests\TestCase;
+
+/**
+ * Class LocationPolicyTest
+ * @package Tests\Integration\Policies\Organization
+ */
+class LocationPolicyTest extends TestCase
+{
+    use DatabaseSetupTrait;
+
+    public function testAllBlocksWhenNotOrganizationManager()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->assertFalse($policy->all($user, $organization));
+    }
+
+    public function testAllPassesForOrganizationManager()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+
+        factory(OrganizationManager::class)->create([
+            'organization_id' => $organization->id,
+            'user_id' => $user->id,
+            'role_id' => Role::ORGANIZATION_MANAGER,
+        ]);
+
+        $this->assertTrue($policy->all($user, $organization));
+    }
+
+    public function testCreateBlocksWhenNotOrganizationManager()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+
+        $this->assertFalse($policy->create($user, $organization));
+    }
+
+    public function testCreatePassesForOrganizationAdmin()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+
+        factory(OrganizationManager::class)->create([
+            'organization_id' => $organization->id,
+            'user_id' => $user->id,
+            'role_id' => Role::ORGANIZATION_ADMIN,
+        ]);
+
+        $this->assertTrue($policy->create($user, $organization));
+    }
+
+    public function testUpdateBlocksWithOrganizationMismatch()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+        $location = factory(Location::class)->create();
+
+        $this->assertFalse($policy->update($user, $organization, $location));
+    }
+
+    public function testUpdateBlocksWhenNotRelatedToOrganization()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+        $location = factory(Location::class)->create();
+
+        factory(OrganizationManager::class)->create([
+            'organization_id' => $organization->id,
+            'user_id' => $user->id,
+            'role_id' => Role::ORGANIZATION_ADMIN,
+        ]);
+
+        $this->assertFalse($policy->update($user, $organization, $location));
+    }
+
+    public function testUpdatePassesForOrganizationAdmin()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+
+        $location = factory(Location::class)->create([
+            'organization_id' => $organization->id,
+        ]);
+
+        factory(OrganizationManager::class)->create([
+            'organization_id' => $organization->id,
+            'user_id' => $user->id,
+            'role_id' => Role::ORGANIZATION_ADMIN,
+        ]);
+
+        $this->assertTrue($policy->update($user, $organization, $location));
+    }
+
+    public function testDeleteBlocksWithOrganizationMismatch()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+        $location = factory(Location::class)->create();
+
+        $this->assertFalse($policy->delete($user, $organization, $location));
+    }
+
+    public function testDeleteBlocksWhenNotRelatedOrganization()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+        factory(OrganizationManager::class)->create([
+            'user_id' => $user->id,
+            'role_id' => Role::ORGANIZATION_MANAGER,
+        ]);
+
+        $location = factory(Location::class)->create();
+
+        $this->assertFalse($policy->delete($user, $organization, $location));
+    }
+
+    public function testDeletePassesForOrganizationAdmin()
+    {
+        $policy = new LocationPolicy();
+        $organization = factory(Organization::class)->create();
+        $user = factory(User::class)->create();
+
+        factory(OrganizationManager::class)->create([
+            'organization_id' => $organization->id,
+            'user_id' => $user->id,
+            'role_id' => Role::ORGANIZATION_ADMIN,
+        ]);
+
+        $location = factory(Location::class)->create([
+            'organization_id' => $organization->id,
+        ]);
+
+        $this->assertTrue($policy->delete($user, $organization, $location));
+    }
+}
