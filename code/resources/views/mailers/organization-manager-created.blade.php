e777231e487fb0b79295418e79fbac20c579d935
Bryce Meyer <bryce13950@gmail.com>
2020-07-13 02:20:49 +0200

    tested entity upgrade

--- code/resources/views/mailers/organization-manager-created.blade.php
+++ code/resources/views/mailers/organization-manager-created.blade.php
@@ -4,5 +4,12 @@
 
     {{-- Enter email content below. You can embed the variable $organization_name, $organization_role, and $temp_password which will be null if this is an existing user. --}}
 
+    <p>You have been added to the organization {{$organization_name}} on Jengu as a {{$organization_role}}!@if($temp_password) Please follow <a href="https://accept.jengu.app">this link</a> to accept your invitation. Your temporary password is...@endif</p>
+
+    @if($temp_password)
+        <h3>{{$temp_password}}</h3>
+
+        <p>You will need to confirm your account with this email address, and enter a password before you can sign into the app.</p>
+    @endif
 
 @endsection
