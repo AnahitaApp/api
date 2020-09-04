85847f6b9c21a7c0a3e8a88dd51a249afb84305c
Bryce Meyer <bryce13950@gmail.com>
2020-09-04 02:51:44 +0200

    fixed null query bugs

--- code/tests/Feature/Http/Location/Request/LocationRequestIndexTest.php
+++ code/tests/Feature/Http/Location/Request/LocationRequestIndexTest.php
@@ -145,4 +145,37 @@ class LocationRequestIndexTest extends TestCase
                 ]
             ]);
     }
+
+    public function testGetPaginationMobileAppRequest()
+    {
+        $this->actAsUser();
+
+        $location = factory(Location::class)->create();
+
+        factory(OrganizationManager::class)->create([
+            'user_id' => $this->actingAs->id,
+            'organization_id' => $location->organization_id,
+        ]);
+
+        factory(Request::class, 6)->create();
+        factory(Request::class, 15)->create([
+            'location_id' => $location->id,
+        ]);
+
+        $response = $this->json('GET', $this->path . $location->id . '/requests?order[created_at]=ASC&expand[requestedBy]=*&expand[requestedItems]=*&filter[completed_by_id]=null');
+        $response->assertStatus(200);
+        $response->assertJson([
+            'total' => 15,
+            'current_page' => 1,
+            'per_page' => 10,
+            'from' => 1,
+            'to' => 10,
+            'last_page' => 2
+        ])
+            ->assertJsonStructure([
+                'data' => [
+                    '*' =>  array_keys((new Request())->toArray())
+                ]
+            ]);
+    }
 }
